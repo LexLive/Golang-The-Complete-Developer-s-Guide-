841e319d00183eb067a5ac0211621f00d0963351
LexLive <76833027+LexLive@users.noreply.github.com>
2021-01-24 23:25:30 +0700

    Add files via upload

---
+++ DevGuide/14VariableDeclarations/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Ace of Spades" // declare variable
+	card := "Ace of Spades" // assign define new variable
+	// don't need to declare variable
+	// card := "Ace of Spades" Go detect what type of varible assign
+	card = "Five of Diamonds" // assign new value no need := just =
+	fmt.Println(card)
+}
